255bcfc444d749c4fd228685bd02a6d642525c3f
guyuwu <1320055972@qq.com>
2023-10-04 19:46:54 +0800

    add dontaion_api/

---
+++ donation_api/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+var app = express();
+
+app.use(cors());
+
+const connection = mysql.createConnection({
+    host: '81.68.225.238',
+    user: 'root',
+    password: 'eudhjxapnxili1.',
+    database: 'test'
+});
+
+
+connection.connect(err => {
+    if (err) {
+        console.error('Database connection failed:', err);
+    } else {
+        console.log('成功连接数据库');
+    }
+});
+
+app.get('/api/data', (req, res) => {
+    connection.query('SELECT * FROM `donation`', (err, results) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+const port = 3030;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
